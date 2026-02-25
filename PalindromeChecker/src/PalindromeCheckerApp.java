aae752d2541a37ee7520fa0b2ac0b5f7821e6b59
AdityaG346 <adityagupta030406@gmail.com>
2026-02-25 09:38:38 +0530

    UC2: Print a Hardcoded Palindrome Result

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,25 +1,37 @@
+
 /*
  *MAIN CLASS - PalindromeCheckerApp
- *USE CASE 1: Application Entry and Welcome Message
+ *UC2: Print a Hardcoded Palindrome Result
  *
  * Description:
  * This class represents the palindrome checker app
  *
  * At this stage, the application:
- * - Starts execution from the main method
- * - Displays a welcome message
- * - Shoes the current version
+ * - Stores a predefined string
+ * - Determines whether the string is a palindrome
+ * - Displays result on console
  *
- *No palindrome logic is implemented yet
  *
  * @author Aditya Gupta
- * @version 1.0
+ * @version 2.0
  */
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker Management System");
-        System.out.println("Version:1.0");
-        System.out.println("System initialized successfully.");
+        String input = new String("MADAM");
+        String reversed;
+        boolean isPalindrome = true;
+        for(int i=0; i<input.length()/2; i++){
+            if(input.charAt(i) != input.charAt(input.length()-1-i)){
+                isPalindrome=false;
+                break;
+            }
+        }
+        if(isPalindrome){
+            System.out.println("The string is a palindrome.");
+        }
+        else{
+            System.out.println("Not a palindrome.");
+        }
     }
 }
