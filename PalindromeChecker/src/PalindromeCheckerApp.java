96398187b8e0a4b3a37f5c26a67e321923cdc3f8
AdityaG346 <adityagupta030406@gmail.com>
2026-02-25 10:00:46 +0530

    UC7: Queue + Stack Based Palindrome Check

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,41 +1,38 @@
 /*
  * MAIN CLASS - PalindromeCheckerApp
- * UC6: Queue + Stack Based Palindrome Check
+ * UC7: Queue + Stack Based Palindrome Check
  *
  * Description:
  * This class represents the palindrome checker app
  *
  * At this stage, the application:
- * - Validates a palindrome by using both Queue (FIFO)
- * - and Stack (LIFO) data structures to compare characters.
+ * - Validates a palindrome using a Deque by
+ * - comparing front and rear characters directly.
  * - Displays formatted result on console
  *
  *
  * @author Aditya Gupta
- * @version 6.0
+ * @version 7.0
  */
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String input = "civic";
-
-        Queue<Character> queue = new LinkedList<>();
-        Stack<Character> stack = new Stack<>();
+        String input = "refer";
+        Deque<Character> deque = new ArrayDeque<>();
 
         for (char c : input.toCharArray()) {
-            queue.add(c);
-            stack.push(c);
+            deque.add(c);
         }
 
         boolean isPalindrome = true;
 
-        while(!queue.isEmpty()) {
-            if(queue.remove() != stack.pop()){
-                isPalindrome=false;
+        while (deque.size() > 1) {
+
+            if (deque.removeFirst() != deque.removeLast()) {
+                isPalindrome = false;
                 break;
             }
         }
