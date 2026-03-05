d3edff9e5131b18572fa0a59d9a722c3a7007591
AdityaG346 <adityagupta030406@gmail.com>
2026-03-05 08:47:45 +0530

    Use Case 10: Normalized Palindrome Validation

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -34,15 +34,12 @@ public class PalindromeCheckerApp {
 
         String input = "A man a plan a canal Panama";
 
-        // Normalize: remove non-alphanumeric characters and convert to lowercase
         String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
         boolean isPalindrome = true;
 
-        // Compare characters from both ends
         for (int i = 0; i < normalized.length() / 2; i++) {
 
-            // Compare symmetric characters
             if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
                 isPalindrome = false;
                 break;
